d3e381f2cc20d324133fa4278bf4dc23e07647be
Stefan Ziffer <stefan.ziffer@accenture.com>
2020-11-27 14:35:38 +0100

    Finalize docker job

--- scripts/pipeline/publish-docker-images.sh
+++ scripts/pipeline/publish-docker-images.sh
@@ -6,7 +6,7 @@ export TAG=trubudget/"$PROJECT_NAME":"$GITHUB_BRANCH"
 
 docker build --build-arg BUILDTIMESTAMP="$BUILDTIMESTAMP" --build-arg CI_COMMIT_SHA="$GITHUB_SHA" --tag "$TAG" -f Dockerfile .
 
-if [[ "$GITHUB_EVENT_NAME" = "pull_request" ]];
+if [[ "$GITHUB_EVENT_NAME" = "push" ]];# TODO change to pull_reuqest
 then
   echo "$PRIVATE_REGISTRY_PASSWORD" | docker login -u "$PRIVATE_REGISTRY_USERNAME" --password-stdin "$PRIVATE_REGISTRY"
   export TAG_BUILD_PRIVATE="$PRIVATE_REGISTRY_BASE/trubudget/$PROJECT_NAME:t_$GITHUB_RUN_ID"
